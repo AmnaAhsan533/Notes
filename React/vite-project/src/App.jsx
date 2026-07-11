127a9a500d3be6f4e0825cc162be6706b245abdd
Amna Ahsan <amnaahsan533@gmail.com>
2026-07-11 12:46:45 +0000

    Refactor App component to include username display and update import statement in main.jsx

--- React/vite-project/src/App.jsx
+++ React/vite-project/src/App.jsx
@@ -1,11 +1,13 @@
 import Coffee from "./coffee"
 
 function App() {
+  const username = "Amna"
   return (
   <>  
     <Coffee/>  
     {/* we can return only one element
     to return multiple elements, we can return elements inside a main element or fragment(empty element) */}
+    <h2>{username}</h2> {/* we can only inject variables, we can't write expressions inside curly braces */}
     <p>Paragraph</p>
   </>
   )
--- React/vite-project/src/main.jsx
+++ React/vite-project/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
@@ -13,24 +13,30 @@ function MyApp() {
 
 /*
 Can we directly pass reactElement objects rather than functions?
-Since react provides it's own render method which  has a particular syntax that's why our object is not rendering using react render method.
-
+Since react provides it's own render method which has a particular syntax that's why our object is not rendering using react render method.
+ 
 */
 
-const ReactElement = {
-    type: 'a',
-    props: {
-        href: "https://google.com",
-        target: '_blank'
-    },
-    children: 'Click me to visit google'
-}
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: "https://google.com",
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
 
-const anotherElemnt = (
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'click me to visit google'
+) // the react render function accept this syntax
+
+const anotherElement = ( 
     <a href="https://google.com" target='_blank'>Visit Google</a>
 )
 
-ReactDOM.createRoot(document.getElementById('root')).render( 
+createRoot(document.getElementById('root')).render( 
     
-    anotherElement
+    <App />
 )

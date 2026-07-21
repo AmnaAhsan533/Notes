7a8b29f2af233bac9a82ac563e8f7a40aca51544
Amna Ahsan <amnaahsan533@gmail.com>
2026-07-21 08:30:40 +0000

    update

--- React_Projects/tree-view/src/App.jsx
+++ React_Projects/tree-view/src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-
+import TreeView from './index'
+import menus from './data'
 
 function App() {
   return (
-    <>
-    
-    </>
+    <TreeView menus={menus}/>
   )
 }
 
